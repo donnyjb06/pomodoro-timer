060b57f69204c7b2bf1291c465f5102221e9bf08
DonovanJabbar <djabbar51@gmail.com>
2025-02-03 23:35:52 -0700

    feat: create skeleton of Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import styles from "./Header.module.scss";
+
+interface HeaderProps {
+  children: React.ReactNode;
+}
+
+const Header: React.FC<HeaderProps> = ({ children }) => {
+  return (
+    
+    <header className={styles.header}>
+      {children}
+    </header>
+  )
+}
+
+export default Header;
