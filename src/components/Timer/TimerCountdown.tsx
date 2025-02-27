334a9007fad282ba61fa8038beb4d29f411b086f
DonovanJabbar <djabbar51@gmail.com>
2025-02-26 17:01:38 -0700

    hotfix(TimerCountdown): remove "React" import
    - "React" import was preventing the
      build process from completing

--- src/components/Timer/TimerCountdown.tsx
+++ src/components/Timer/TimerCountdown.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { useTimerContext } from "@utils/Timer/TimerContext";
 import styles from "./Timer.module.scss";
 import useMediaQuery from "@utils/useMediaQuery";
